7fba4ba3360c2071e07004cfc8ff0392b63a5e42
dunglt <luuthidungtl@gmail.com>
2025-04-17 13:34:12 +0700

    fix: build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
     "./apps/**/*.{vue,js,ts,jsx,tsx}",
     "../../libs/ui/src/**/*.{vue,js,ts,jsx,tsx}"
   ],
